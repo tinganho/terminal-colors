03c7f177d36f37e2759d1e5ccca8f59a70be3eb8
Tingan Ho <tingan87@gmail.com>
2014-01-26 06:23:31 +0800

    Added more color mapping

--- index.js
+++ index.js
@@ -52,15 +52,22 @@ function _stylize(str, style) {
     //background colors
     //grayscale
     'whiteBG' : ['\x1B[47m', '\x1B[49m'],
-    'greyBG' : ['\x1B[49;5;8m', '\x1B[49m'],
+    'lightWhiteBG' : ['\x1B[107m', '\x1B[49m'],
     'blackBG' : ['\x1B[40m', '\x1B[49m'],
+    'lightBlackBG' : ['\x1B[100m', '\x1B[49m'],
     //colors
     'blueBG' : ['\x1B[44m', '\x1B[49m'],
+    'lightBlueBG' : ['\x1B[104m', '\x1B[49m'],
     'cyanBG' : ['\x1B[46m', '\x1B[49m'],
+    'lightCyanBG' : ['\x1B[106m', '\x1B[49m'],
     'greenBG' : ['\x1B[42m', '\x1B[49m'],
+    'lightGreenBG' : ['\x1B[106m', '\x1B[49m'],
     'magentaBG' : ['\x1B[45m', '\x1B[49m'],
+    'lightMagentaBG' : ['\x1B[105m', '\x1B[49m'],
     'redBG' : ['\x1B[41m', '\x1B[49m'],
-    'yellowBG' : ['\x1B[43m', '\x1B[49m']
+    'lightRedBG' : ['\x1B[101m', '\x1B[49m'],
+    'yellowBG' : ['\x1B[43m', '\x1B[49m'],
+    'lightYellowBG' : ['\x1B[103m', '\x1B[49m'],
   };
 
   return styles[style][0] + str + styles[style][1];
@@ -92,15 +99,22 @@ var names = [
   'lightCyan',
   'magenta',
   'lightMagenta',
-  'greyBG',
   'blackBG',
+  'lightBlackBG',
   'yellowBG',
+  'lightYellowBG',
   'redBG',
+  'lightRedBG',
   'greenBG',
+  'lightGreenBG',
   'blueBG',
+  'lightBlueBG',
   'whiteBG',
+  'lightWhiteBG',
   'cyanBG',
-  'magentaBG'
+  'lightCyanBG',
+  'magentaBG',
+  'lightMagentaBG'
 ];
 
 /**
